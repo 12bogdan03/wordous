58081fe5c3a761390bf8d20443e105d8fd536357
12bogdan03 <12bogdan03@gmail.com>
2018-02-22 20:34:41 +0000

    change static root

--- wordous/settings.py
+++ wordous/settings.py
@@ -136,7 +136,7 @@ LOGOUT_URL = reverse_lazy('accounts:logout')
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/home/deodatus/wordous_static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
