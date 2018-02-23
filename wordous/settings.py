0b6339ddbae58c6118d2ace2e6c6d0194c7081dd
12bogdan03 <12bogdan03@gmail.com>
2018-02-23 18:43:49 +0200

    change settings for https

--- wordous/settings.py
+++ wordous/settings.py
@@ -16,6 +16,10 @@ DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 MESSAGE_TAGS = {
     messages.DEBUG: 'alert-info',
